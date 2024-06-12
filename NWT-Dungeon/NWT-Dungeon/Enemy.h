f406fa40a2bd3a3fb31d4aff02261cc1cb6fc9c6
ujn007 <hyunjun9531@gmail.com>
2024-06-12 14:11:09 +0900

    ..

--- NWT-Dungeon/NWT-Dungeon/Enemy.h
+++ NWT-Dungeon/NWT-Dungeon/Enemy.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Core.h"
+#include "console.h"
+
 class Enemy
 {
 public:
--- NWT-Dungeon/NWT-Dungeon/console.h
+++ NWT-Dungeon/NWT-Dungeon/console.h
@@ -2,14 +2,14 @@
 #include <winnt.h>
 void FullScreen();
 void GotoXY(int x, int y);
-BOOL GotoPos(int x, int y);
+BOOL GotoPos(int x, int y);	
 COORD CursorPosition();
-void CursorVisible(bool visible, DWORD size);
+void CursorVisible(bool visible, DWORD size);					
 void SetColor(int textColor = 15, int bgColor = 0);
 int GetColor();
 void LockResize();
-COORD GetConsoleResolution();
-void SetFontSize(UINT weight, UINT fontX, UINT fontY);
+COORD GetConsoleResolution();	
+void SetFontSize(UINT weight, UINT fontX, UINT fontY);	
 enum class Color {
 	Black, Blue, Green, Skyblue,
 	Red, Violet, Yellow, LightGray,
